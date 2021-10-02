2d776ab95900a8344c5770b0834ee0bc548f1552
Aravind Putrevu <aravind.putrevu@gmail.com>
2021-10-02 22:34:57 +0530

    Add Search UI

--- pages/ingest.js
+++ pages/ingest.js
@@ -39,7 +39,8 @@ export function DataIngested(props){
   return ( 
         <div className="text-center text-2xl">
             <p className="break-all tracking-normal max-w-xl leading-loose"> 
-                🎉 You've ingested <span className="animate-bounce text-red-800 font-bold">{props.docCount}</span> documents into Elasticsearch!
+                🎉 You've ingested <span className="animate-bounce text-red-800 font-bold">{props.docCount}</span> documents into Elasticsearch! 
+                <a href="/search" className="inline-block px-5 py-3 rounded-lg shadow-lg bg-blue-450 text-white tracking-wider font-semibold text-lg">Search 🔍</a>
             </p> 
         </div> 
         );
