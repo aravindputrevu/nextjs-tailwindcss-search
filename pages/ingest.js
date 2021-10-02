9cf7f583de9ed7dbcd4b7eb71b7c45247e3d7e31
Aravind Putrevu <aravind.putrevu@gmail.com>
2021-10-02 22:32:45 +0530

    Ingest data to ES, adding custom utility class

--- pages/ingest.js
+++ pages/ingest.js
@@ -1,7 +1,15 @@
 import Head from 'next/head'
 import { isConnectedToESS } from '../lib/elasticsearch'
 
-export default function ingest() {
+export default function ingest({isConnected, dataIngested, docCount}) {
+
+  const ingestData = async event => {
+    event.preventDefault()
+    const res = await fetch('/api/ingestData', {
+      method: 'POST'
+    })
+    await res.json()
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -9,12 +17,45 @@ export default function ingest() {
         <title>Ingesting data into Elasticsearch</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <p className="text-center">You're connected to Elastic Cloud!</p>
-      </div>
+      {(isConnected && dataIngested) && <DataIngested docCount={docCount}/>}
+      {(isConnected===true && dataIngested===false) && <IngestDataIntoESS ingestData={ingestData}/> }
+      {(isConnected=== false) && <ConfigureESS/>}
     </div>
   )
-  }
+}
+
+export function IngestDataIntoESS(props) {
+  return <div>
+  <p className="text-center">You're connected to Elastic Cloud! Hit below button to ingest sample data.</p>
+  <div className="text-center pt-2">
+    <a href="/api/ingestData" className="inline-block px-5 py-3 rounded-lg shadow-lg bg-poppy text-black tracking-wider font-semibold text-sm">
+      Ingest!
+    </a>
+  </div>
+</div>;
+}
+
+export function DataIngested(props){
+  return ( 
+        <div className="text-center text-2xl">
+            <p className="break-all tracking-normal max-w-xl leading-loose"> 
+                🎉 You've ingested <span className="animate-bounce text-red-800 font-bold">{props.docCount}</span> documents into Elasticsearch!
+            </p> 
+        </div> 
+        );
+
+}
+
+export function ConfigureESS() {
+  return <div className="">
+  <p className="text-2xl">
+    It seems you have not configured Elasticsearch!
+    <p className="p-20 pt-2 text-xl">
+      Get started by editing <code className="pl-1 pr-1 bg-gray-200 rounded-md">env.local</code>
+    </p>
+  </p>
+</div>;
+}
 
 export async function getServerSideProps() {
     return isConnectedToESS()
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          450: '#0077CC'
+        },
+        'poppy': '#FA744E',
+      }
+    },
   },
   variants: {
     extend: {},
