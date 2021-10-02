a7db9794b646e0dd124800c6584f4a17a5ce6695
Aravind Putrevu <aravind.putrevu@gmail.com>
2021-10-02 22:08:36 +0530

    Final APIs & Page changes

--- pages/ingest.js
+++ pages/ingest.js
@@ -1,37 +1,20 @@
 import Head from 'next/head'
-import { IngestDataIntoESS, ConfigureESS, isConnectedToESS } from '../lib/elasticsearch'
+import { isConnectedToESS } from '../lib/elasticsearch'
 
-export default function ingest({isConnected, dataIngested, docCount}) {
-  console.log('Test Test variables ***', isConnected, docCount, dataIngested)
+export default function ingest() {
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Head>
         <title>Ingesting data into Elasticsearch</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {(isConnected && dataIngested) && <DataIngested docCount={docCount}/>}
-      {(isConnected===true && dataIngested===false) && <IngestDataIntoESS/> }
-      {(isConnected=== false) && <ConfigureESS/>}
+      <div>
+        <p className="text-center">You're connected to Elastic Cloud!</p>
+      </div>
     </div>
   )
-}
-
-
-
-function DataIngested(props){
-  console.log("Data Ingested")
-  return ( 
-        <div className="text-center text-2xl">
-            <p className="break-all tracking-normal max-w-xl leading-loose"> 
-                🎉 You've ingested <span className="animate-bounce text-red-800 font-bold">{props.docCount}</span> documents into Elasticsearch! 
-                <a href="/search" className="inline-block px-5 py-3 rounded-lg shadow-lg bg-blue-450 text-white tracking-wider font-semibold text-lg">
-                   Search 🔍
-                </a>
-            </p> 
-        </div> 
-        );
-
-}
+  }
 
 export async function getServerSideProps() {
     return isConnectedToESS()
